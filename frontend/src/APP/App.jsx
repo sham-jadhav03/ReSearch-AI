940a23c4b0cece39d7f1d71b3debd67951f363aa
sham jadhav <ghanshamjadhav2003@gmail.com>
2026-03-24 15:30:08 +0530

    add user authentication pages and routing with Login and Register components

--- frontend/src/APP/App.jsx
+++ frontend/src/APP/App.jsx
@@ -1,12 +1,12 @@
-
+import { RouterProvider } from "react-router";
+import { router } from "./routes/AppRoutes.jsx";
 
 function App() {
-
   return (
     <>
-  
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/features/chat/components/Protected.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Protected = () => {
+  return (
+    <div>protected</div>
+  )
+}
+
+export default Protected
