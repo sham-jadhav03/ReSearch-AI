745a05a57d4e0dfd1f95560a8493714a6454e10c
sham jadhav <ghanshamjadhav2003@gmail.com>
2026-03-26 19:31:46 +0530

    add AI service integration with Google Generative AI and Mistral models

---
+++ backend/src/services/ai.service.js
@@ -0,0 +1,45 @@
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { ChatMistralAI } from "@langchain/mistralai";
+import { HumanMessage, SystemMessage, AIMessage } from "langchain";
+
+const geminiModel = new ChatGoogleGenerativeAI({
+  model: "gemini-2.5-flash-lite",
+  apiKey: process.env.GEMINI_API_KEY,
+});
+
+const mistralModel = new ChatMistralAI({
+  model: "mistral-small-latest",
+  apiKey: process.env.MISTRAL_API_KEY,
+});
+
+export const generateResponse = async (message) => {
+  console.log(message);
+
+  const response = await geminiModel.invoke(
+    message.map((msg) => {
+      if (msg.role == "user") {
+        return new HumanMessage(msg.content);
+      } else if (msg.role == "ai") {
+        return new AIMessage(msg.content);
+      }
+    }),
+  );
+
+  return response.text;
+};
+
+export const generateChatTitle = async (message) => {
+  const response = await mistralModel.invoke([
+    new SystemMessage(`
+            You are a helpful assistant that generates concise and description title for chat conversations.
+
+            User will provide you with the first message of a chat conversation, and you will generate a title that captures the essence of the conversation in 2-4 words. The title should be clear, relevant, and engaging, giving users a quick understanding of the chat's topic.    
+        `),
+    new HumanMessage(`
+            Generate a title for a chat conversation based on the following first message:
+            "${message}"
+            `),
+  ]);
+
+  return response.text;
+};
